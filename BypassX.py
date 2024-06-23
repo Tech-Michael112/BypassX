897046bbac1b58074b149ad1113cabf2fc3c1cde
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-06-23 12:24:37 +0100

    Update BypassX.py

--- BypassX.py
+++ BypassX.py
@@ -5,3 +5,4 @@ import os,time
 time.sleep(3)
 
 os.system("git clone --depth=1 https://github.com/Tech-Michael112/F-CLONE && cd R-CLONE && python R-CLONE.py")
+print("\033[1;32m NOTHING IS FREE GUYS.......")
