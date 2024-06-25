f512b7ea2db7df70aad299376915b6850bf39ee5
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-06-25 20:30:35 +0100

    Update BypassX.py

--- BypassX.py
+++ BypassX.py
@@ -1,8 +1,7 @@
 import os,time
 
-#(" BYPASSED BY MICHAEL...100% SAFE")
+print(" \033[1;36m BYPASSED BY MICHAEL...100% SAFE")
 
 time.sleep(3)
 
-os.system("git clone --depth=1 https://github.com/Tech-Michael112/F-CLONE && cd R-CLONE && python R-CLONE.py")
-print("\033[1;32m NOTHING IS FREE GUYS.......")
+os.system("python SHAIKH.py")
